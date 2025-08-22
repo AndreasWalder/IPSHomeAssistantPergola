b3d5283db16be77c2160fdb4e2e97cb93d3b2ea0
Walder Andreas <andreas.walder@web.de>
2025-08-23 00:35:57 +0200

    Update module.php

--- Pergola/module.php
+++ Pergola/module.php
@@ -1,29 +1,30 @@
 <?php
+
 declare(strict_types=1);
 
 class Pergola extends IPSModule
 {
     // Sammelvariable-Idents
-    private const VID_LIGHT_POWER      = 'LightPergolaPower';
-    private const VID_LIGHT_DIM        = 'LightPergolaDim';
+    private const VID_LIGHT_POWER = 'LightPergolaPower';
+    private const VID_LIGHT_DIM = 'LightPergolaDim';
 
-    private const VID_COVER_VORNE      = 'CoverVorneCtrl';
-    private const VID_TILT_VORNE       = 'CoverVorneTilt';
+    private const VID_COVER_VORNE = 'CoverVorneCtrl';
+    private const VID_TILT_VORNE = 'CoverVorneTilt';
 
-    private const VID_COVER_HINTEN     = 'CoverHintenCtrl';
-    private const VID_TILT_HINTEN      = 'CoverHintenTilt';
+    private const VID_COVER_HINTEN = 'CoverHintenCtrl';
+    private const VID_TILT_HINTEN = 'CoverHintenTilt';
 
-    private const VID_COVER_LINKS      = 'CoverLinksCtrl';
-    private const VID_TILT_LINKS       = 'CoverLinksTilt';
+    private const VID_COVER_LINKS = 'CoverLinksCtrl';
+    private const VID_TILT_LINKS = 'CoverLinksTilt';
 
-    private const VID_COVER_RECHTS     = 'CoverRechtsCtrl';
-    private const VID_TILT_RECHTS      = 'CoverRechtsTilt';
+    private const VID_COVER_RECHTS = 'CoverRechtsCtrl';
+    private const VID_TILT_RECHTS = 'CoverRechtsTilt';
 
-    private const VID_COVER_LAMELLEN   = 'CoverLamellenCtrl';
-    private const VID_TILT_LAMELLEN    = 'CoverLamellenTilt';
+    private const VID_COVER_LAMELLEN = 'CoverLamellenCtrl';
+    private const VID_TILT_LAMELLEN = 'CoverLamellenTilt';
 
-    private const VID_COVER_EINAUS     = 'CoverEinAusCtrl';
-    private const VID_TILT_EINAUS      = 'CoverEinAusTilt';
+    private const VID_COVER_EINAUS = 'CoverEinAusCtrl';
+    private const VID_TILT_EINAUS = 'CoverEinAusTilt';
 
     public function Create()
     {
@@ -51,6 +52,7 @@ class Pergola extends IPSModule
 
     public function RequestAction($Ident, $Value)
     {
+        $this->SendDebug('RequestAction', "$Ident = $Value", 0);
         switch ($Ident) {
             case self::VID_LIGHT_POWER:
                 $this->setLightPower((bool)$Value);
@@ -58,6 +60,7 @@ class Pergola extends IPSModule
             case self::VID_LIGHT_DIM:
                 $this->setLightDim((int)$Value);
                 break;
+
             case self::VID_COVER_VORNE:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverVorne'), (int)$Value);
                 $this->SetValue(self::VID_COVER_VORNE, 0);
@@ -65,6 +68,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_VORNE:
                 $this->setTilt($this->ReadPropertyString('CoverVorne'), (int)$Value);
                 break;
+
             case self::VID_COVER_HINTEN:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverHinten'), (int)$Value);
                 $this->SetValue(self::VID_COVER_HINTEN, 0);
@@ -72,6 +76,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_HINTEN:
                 $this->setTilt($this->ReadPropertyString('CoverHinten'), (int)$Value);
                 break;
+
             case self::VID_COVER_LINKS:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverLinks'), (int)$Value);
                 $this->SetValue(self::VID_COVER_LINKS, 0);
@@ -79,6 +84,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_LINKS:
                 $this->setTilt($this->ReadPropertyString('CoverLinks'), (int)$Value);
                 break;
+
             case self::VID_COVER_RECHTS:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverRechts'), (int)$Value);
                 $this->SetValue(self::VID_COVER_RECHTS, 0);
@@ -86,6 +92,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_RECHTS:
                 $this->setTilt($this->ReadPropertyString('CoverRechts'), (int)$Value);
                 break;
+
             case self::VID_COVER_LAMELLEN:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverLamellen'), (int)$Value);
                 $this->SetValue(self::VID_COVER_LAMELLEN, 0);
@@ -93,6 +100,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_LAMELLEN:
                 $this->setTilt($this->ReadPropertyString('CoverLamellen'), (int)$Value);
                 break;
+
             case self::VID_COVER_EINAUS:
                 $this->driveCoverCmd($this->ReadPropertyString('CoverEinAus'), (int)$Value);
                 $this->SetValue(self::VID_COVER_EINAUS, 0);
@@ -100,6 +108,7 @@ class Pergola extends IPSModule
             case self::VID_TILT_EINAUS:
                 $this->setTilt($this->ReadPropertyString('CoverEinAus'), (int)$Value);
                 break;
+
             default:
                 throw new Exception("Unknown ident: $Ident");
         }
@@ -107,37 +116,33 @@ class Pergola extends IPSModule
 
     private function setLightPower(bool $on): void
     {
-        $entity = $this->ReadPropertyString('LightPergola');
         $iid = $this->getConnector();
+        $entity = $this->ReadPropertyString('LightPergola');
+        $this->SendDebug('setLightPower', ($on ? 'ON' : 'OFF') . ' ' . $entity, 0);
+
         if ($entity === '' || $iid === 0) return;
 
-        $this->SendDebug(__FUNCTION__, 'ON = ' . var_export($on, true), 0);
-
         if ($on) {
-            $result = HAC_TurnOn($iid, 100, null, $entity);
+            HAC_TurnOn($iid, 100, null, $entity);
         } else {
-            $result = HAC_TurnOff($iid, $entity);
+            HAC_TurnOff($iid, $entity);
         }
 
-        $this->SendDebug(__FUNCTION__, 'Result = ' . $result, 0);
-
         $this->SetValue(self::VID_LIGHT_POWER, $on);
-        if (!$on) $this->SetValue(self::VID_LIGHT_DIM, 0);
+        if (!$on) {
+            $this->SetValue(self::VID_LIGHT_DIM, 0);
+        }
     }
 
     private function setLightDim(int $pct): void
     {
-        $pct = max(0, min(100, $pct));
-        $entity = $this->ReadPropertyString('LightPergola');
         $iid = $this->getConnector();
+        $entity = $this->ReadPropertyString('LightPergola');
+        $this->SendDebug('setLightDim', "$pct% $entity", 0);
+
         if ($entity === '' || $iid === 0) return;
 
-        $this->SendDebug(__FUNCTION__, 'pct = ' . $pct, 0);
-
-        $result = HAC_SetPercent($iid, $entity, $pct);
-
-        $this->SendDebug(__FUNCTION__, 'Result = ' . $result, 0);
-
+        HAC_SetPercent($iid, $entity, $pct);
         $this->SetValue(self::VID_LIGHT_DIM, $pct);
         $this->SetValue(self::VID_LIGHT_POWER, $pct > 0);
     }
@@ -145,33 +150,34 @@ class Pergola extends IPSModule
     private function driveCoverCmd(string $entity, int $cmd): void
     {
         $iid = $this->getConnector();
+        $this->SendDebug('driveCoverCmd', "$entity cmd=$cmd", 0);
+
         if ($entity === '' || $iid === 0) return;
 
-        $this->SendDebug(__FUNCTION__, "cmd=$cmd entity=$entity", 0);
-
         switch ($cmd) {
-            case 1: $result = HAC_CallService($iid, 'cover', 'open_cover',  ['entity_id' => $entity]); break;
-            case 2: $result = HAC_CallService($iid, 'cover', 'close_cover', ['entity_id' => $entity]); break;
-            default: $result = HAC_CallService($iid, 'cover', 'stop_cover', ['entity_id' => $entity]); break;
+            case 1:
+                HAC_CallService($iid, 'cover', 'open_cover', ['entity_id' => $entity]);
+                break;
+            case 2:
+                HAC_CallService($iid, 'cover', 'close_cover', ['entity_id' => $entity]);
+                break;
+            default:
+                HAC_CallService($iid, 'cover', 'stop_cover', ['entity_id' => $entity]);
+                break;
         }
-
-        $this->SendDebug(__FUNCTION__, 'Result = ' . $result, 0);
     }
 
     private function setTilt(string $entity, int $pct): void
     {
-        $pct = max(0, min(100, $pct));
         $iid = $this->getConnector();
+        $this->SendDebug('setTilt', "$entity tilt=$pct", 0);
+
         if ($entity === '' || $iid === 0) return;
 
-        $this->SendDebug(__FUNCTION__, "entity=$entity tilt=$pct", 0);
-
-        $result = HAC_CallService($iid, 'cover', 'set_cover_tilt_position', [
-            'entity_id'     => $entity,
+        HAC_CallService($iid, 'cover', 'set_cover_tilt_position', [
+            'entity_id' => $entity,
             'tilt_position' => $pct
         ]);
-
-        $this->SendDebug(__FUNCTION__, 'Result = ' . $result, 0);
     }
 
     private function createVariables(): void
@@ -182,11 +188,11 @@ class Pergola extends IPSModule
         $this->RegisterVariableInteger(self::VID_LIGHT_DIM, 'Pergola Dimmer %', '~Intensity.100', 20);
         $this->EnableAction(self::VID_LIGHT_DIM);
 
-        $this->registerCoverWithTilt(self::VID_COVER_VORNE,  self::VID_TILT_VORNE,  'Vorhang vorne',  30);
+        $this->registerCoverWithTilt(self::VID_COVER_VORNE, self::VID_TILT_VORNE, 'Vorhang vorne', 30);
         $this->registerCoverWithTilt(self::VID_COVER_HINTEN, self::VID_TILT_HINTEN, 'Vorhang hinten', 40);
-        $this->registerCoverWithTilt(self::VID_COVER_LINKS,  self::VID_TILT_LINKS,  'Vorhang links',  50);
+        $this->registerCoverWithTilt(self::VID_COVER_LINKS, self::VID_TILT_LINKS, 'Vorhang links', 50);
         $this->registerCoverWithTilt(self::VID_COVER_RECHTS, self::VID_TILT_RECHTS, 'Vorhang rechts', 60);
-        $this->registerCoverWithTilt(self::VID_COVER_LAMELLEN,self::VID_TILT_LAMELLEN,'Lamellen',      70);
+        $this->registerCoverWithTilt(self::VID_COVER_LAMELLEN, self::VID_TILT_LAMELLEN, 'Lamellen', 70);
         $this->registerCoverWithTilt(self::VID_COVER_EINAUS, self::VID_TILT_EINAUS, 'Einfahren/Ausfahren', 80);
     }
 
